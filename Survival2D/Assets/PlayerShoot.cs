115bf71eeac5f9e5565da510bd5b0f42d7024adb
iGNiTe <ignarmezh@gmail.com>
2018-04-06 23:29:30 +0300

    Tried to fix bug with network again

--- Survival2D/Assets/PlayerShoot.cs
+++ Survival2D/Assets/PlayerShoot.cs
@@ -2,44 +2,51 @@
 using UnityEngine;
 using UnityEngine.Networking;
 
-public class PlayerShoot : NetworkBehaviour {
+public class PlayerShoot : NetworkBehaviour
+{
 
     public PlayerWeapon playerWeapon;
 
     public GameObject bulletPrefab;
-    
+
+    [SerializeField]
     Transform bulletSpawn;
-    
-	void Start () {
-        if (isLocalPlayer)
+
+    void Start()
+    {
+        foreach (Transform child in transform)
         {
-            foreach (Transform child in transform)
+            foreach (Transform item in child)
             {
-                foreach (Transform item in child)
+                if (item.name == "BulletSpawn")
                 {
-                    if (item.name == "BulletSpawn")
-                    {
-                        bulletSpawn = item;
-                    }
+                    bulletSpawn = item;
                 }
             }
         }
 
     }
-	
-	void Update () {
+
+    void Update()
+    {
         if (Input.GetButtonDown("Fire1"))
         {
-            CmdShoot();
+            CmdShot();
         }
-	}
+    }
 
     //1) [ClientRpc] функции выполняются на всех клиентах, но их можно запускать ТОЛЬКО с сервера.
     //2) [Command] функции выполняются только на сервере, но ВЫЗВАТЬ ее можно на клиенте и только через объект который помечен как isLocalPlayer.
 
     [Command]
-    void CmdShoot()
+    void CmdShot()
     {
+        Debug.Log("SHOOT");
+        if (bulletSpawn == null)
+        {
+            Debug.Log("bulletSpawn == null");
+            return;
+        }
         GameObject bullet = Instantiate(bulletPrefab,bulletSpawn.transform.position,Quaternion.identity);
         Rigidbody2D bulletrb = bullet.GetComponent<Rigidbody2D>();
         bulletrb.AddForce(bulletSpawn.transform.up * 10f,ForceMode2D.Impulse);
